b4f55da6fc774cd055815b660ad059bb876020a5
DamugeDev <damugemusda@gmail.com>
2022-07-29 14:32:45 +0200

    relogio

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -451,3 +451,16 @@
         clearTimeout(id);
       };
 }());
+
+
+function relogio(){
+  var data= new Date();
+  var hr=data.getHours();
+  var min = data.getMinutes();
+  var s= data.getSeconds();
+
+  var tempo_total= hr + ":" + min+ ":" + s;
+  var tempo=window.document.getElementById("div_relogio");
+  tempo.innerHTML=tempo_total;
+}
+setInterval(relogio,500);
